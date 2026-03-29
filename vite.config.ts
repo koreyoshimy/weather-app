8661826380c4254f2b27f2190523d90611c969a4
Foo Xin Chee <cheeoo0818@gmail.com>
2026-03-29 17:31:38 +0800

    save local config fixes

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,12 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/-weather-app/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
+        
       },
     },
     server: {
